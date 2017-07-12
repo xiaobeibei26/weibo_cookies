6405f6e70c9aff421f85ad89c29fef6cca3cace3
xiaobeibei26 <xiaobeibei26@gmail.com>
2017-07-12 11:11:05 +0800

    Update settings.py

--- cookie_pool/settings.py
+++ cookie_pool/settings.py
@@ -2,9 +2,9 @@
 
 DEFAULT_BROWSER='Chrome'#默认用于登陆的浏览器
 
-# 云打码相关配置到yundama.com申请注册
-YUNDAMA_USERNAME = 'wweballl'
-YUNDAMA_PASSWORD = 'abc13421224262'
+# 云打码相关配置到yundama.com申请注册，这个是开发者账号
+YUNDAMA_USERNAME = ''#用户名
+YUNDAMA_PASSWORD = ''#密码
 YUNDAMA_APP_ID = '3523'
 YUNDAMA_APP_KEY = '6916d827de5e35e6e807c39bdaeab49d'
 
